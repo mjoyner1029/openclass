5fcf2fd01cca8bea98afdb31b9e296cf8be9a4cc
mjoyner1029 <mjoyner18@gmail.com>
2024-04-12 17:32:10 -0700

    Added my project

---
+++ code_correction2.py
@@ -0,0 +1,4 @@
+Pi_value = 3.14
+userAge = 25
+user_location = "New York"
+MAXLIMIT = 1000
---
+++ operations.py
@@ -0,0 +1,19 @@
+# task 1
+a = 5
+b = 8
+c = 7
+
+result = a * b - c + a
+print(result)
+
+results = (a *( b - c) + a)
+print(results)
+
+# task 2
+a = 2 + 4
+b = 3 + 7
+
+if a > b:
+    print('a is larger than b')
+
+
