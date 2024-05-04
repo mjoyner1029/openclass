776fea1a1ec9d4dd59c194a1cc9828ef82f8486c
mjoyner1029 <mjoyner18@gmail.com>
2024-05-03 17:06:28 -0700

    Added my project

---
+++ Python Loop Statements/5_looping_lists.py
@@ -0,0 +1,39 @@
+# task 1
+genres = ['Jazz', 'Rock', 'Hip-hop', 'Classical']
+count = genres
+
+
+for genre in genres:
+    count =+1
+    if genre == "Jazz":
+        print(f"{count} Jazz from the city of New Orleans")
+    if genre == "Rock":
+        print(f"{count} Rock and roll baby!")
+    if genre == "Hip-hop":
+        print(f"{count} All the MC's stand up!")
+    if genre == "Classical":
+        print(f"{count} Relax to some classical music")
+
+# task 2 while 
+while True:
+    if genre == "Jazz":
+        print(f"{count} Jazz from the city of New Orleans")
+    if genre == "Rock":
+        print(f"{count} Rock and roll baby!")
+    if genre == "Classical":
+        print(f"{count} Relax to some classical music")
+    else:
+        print("All the MC's stand up!")
+        break
+
+# task 3 range()
+for genre in genres:
+    count =+1
+    if genre == "Jazz":
+        print(f"{count} Jazz from the city of New Orleans")
+    if genre == "Rock":
+        print(f"{count} Rock and roll baby!")
+    if genre == "Hip-hop":
+        print(f"{count} All the MC's stand up!")
+    if genre == "Classical":
+        print(f"{count} Relax to some classical music")
