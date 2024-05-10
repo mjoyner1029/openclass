a53ba7c9d54bb3a6cdf8f67ee9fefe195b543b0e
mjoyner1029 <mjoyner18@gmail.com>
2024-05-09 20:18:02 -0700

    Added my project

--- Python Loop Statements/5_looping_lists.py
+++ Python Loop Statements/5_looping_lists.py
@@ -1,10 +1,10 @@
 # task 1
 genres = ['Jazz', 'Rock', 'Hip-hop', 'Classical']
-count = genres
+count = 0
 
 
 for genre in genres:
-    count =+1
+    count += 1
     if genre == "Jazz":
         print(f"{count} Jazz from the city of New Orleans")
     if genre == "Rock":
@@ -15,25 +15,25 @@ for genre in genres:
         print(f"{count} Relax to some classical music")
 
 # task 2 while 
+count = 0
+
 while True:
+    genre = genres[count]
+    count += 1
     if genre == "Jazz":
         print(f"{count} Jazz from the city of New Orleans")
-    if genre == "Rock":
+    elif genre == "Rock":
         print(f"{count} Rock and roll baby!")
-    if genre == "Classical":
+    elif genre == "Classical":
         print(f"{count} Relax to some classical music")
     else:
-        print("All the MC's stand up!")
         break
+print("All the MC's stand up!")
+        
 
-# task 3 range()
-for genre in genres:
-    count =+1
-    if genre == "Jazz":
-        print(f"{count} Jazz from the city of New Orleans")
-    if genre == "Rock":
-        print(f"{count} Rock and roll baby!")
-    if genre == "Hip-hop":
-        print(f"{count} All the MC's stand up!")
-    if genre == "Classical":
-        print(f"{count} Relax to some classical music")
+# task 3
+for i in range(len(genres)):
+    if genres[i] != "Classical":
+        print(f"Genre {genres[i]} is suitable for the light show!")
+    else:
+        print(f"{genres[i]} is so boring it doesnt need lights")
